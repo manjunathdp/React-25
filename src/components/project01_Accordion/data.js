7ebee77a3e387d4bff069cd0f0afdc06990ca7cd
Manjunath Pujar <manjunathdtc3@gmail.com>
2025-01-04 14:35:40 +0530

    dummy data created

---
+++ src/components/project01_Accordion/data.js
@@ -0,0 +1,14 @@
+const data = [
+  { id: "1", question: "what is your name", answer: "my name is manjunath" },
+  {
+    id: "2",
+    question: "what is your father name",
+    answer: "my father name is xyz",
+  },
+  {
+    id: "3",
+    question: "what is your mother name",
+    answer: "my mother name is abc",
+  },
+];
+ export default data;
