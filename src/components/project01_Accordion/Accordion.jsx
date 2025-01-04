8731b6a4639d0f6ce85dd6e22df5ed5bd0e2017a
Manjunath Pujar <manjunathdtc3@gmail.com>
2025-01-04 19:45:28 +0530

    Accordion project completed

--- src/components/project01_Accordion/Accordion.jsx
+++ src/components/project01_Accordion/Accordion.jsx
@@ -4,20 +4,46 @@ import data from "./data";
 
 export default function Accordion() {
   const [selected, setSelected] = useState(null);
-  function handelSelection(id) {
+  const [enableMultiselect, setEnableMultiselect] = useState(false);
+  const [multiple, setMultiple] = useState([]);
+  function handelSingleSelection(id) {
     if (id === selected) {
       setSelected(null);
     } else setSelected(id);
   }
+
+  function handelMultipleSelection(id) {
+    let copyMultiple = [...multiple];
+    const currentIndex = copyMultiple.indexOf(id);
+
+    console.log(currentIndex);
+    if (currentIndex === -1) {
+      copyMultiple.push(id);
+    } else {
+      copyMultiple.splice(currentIndex, 1);
+    }
+    setMultiple(copyMultiple);
+  }
+  console.log("multiple: ", multiple);
   return (
     <div>
+      <button onClick={() => setEnableMultiselect(!enableMultiselect)}>
+        Enable Multi Selection
+      </button>
       {data.map((dataItem) => (
         <div>
-          <div onClick={() => handelSelection(dataItem.id)}>
+          <div
+            onClick={
+              enableMultiselect
+                ? () => handelMultipleSelection(dataItem.id)
+                : () => handelSingleSelection(dataItem.id)
+            }
+          >
             <h3>{dataItem.question}</h3>
           </div>
           <div>
-            {selected === dataItem.id ? (
+            {selected === dataItem.id ||
+            multiple.indexOf(dataItem.id) !== -1 ? (
               <p>{dataItem.answer}</p>
             ) : (
               <p>{null}</p>
